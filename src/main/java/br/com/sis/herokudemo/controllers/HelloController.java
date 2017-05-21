7b17d026fe86565ba89af603922de4d63433fc76
Sérgio Magalhães do Nascimento <work-sergio@hotmail.com>
2017-05-21 11:43:31 -0300

    Teste JSON

--- src/main/java/br/com/sis/herokudemo/controllers/HelloController.java
+++ src/main/java/br/com/sis/herokudemo/controllers/HelloController.java
@@ -1,5 +1,8 @@
 package br.com.sis.herokudemo.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +13,17 @@ public class HelloController {
 	
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String hello()
+	public List<String> hello()
 	{
-		return "Hey Chuchi  eu te amo :3 <3";
+		List<String> cursos = new ArrayList<>();
+		cursos.add("Java 8");
+		cursos.add("Angular");
+		cursos.add("Spring");
+		cursos.add("BootStrap");
+		cursos.add("Docker");
+		
+		
+		return cursos;
 	}
 
 }
