ff6cb64fcb7c28d06fcd2bea359d0be8092dc50b
Sérgio Magalhães do Nascimento <work-sergio@hotmail.com>
2017-05-25 22:14:43 -0300

    prepara saida de json para aplicações externas

--- src/main/java/br/com/sis/herokudemo/controllers/CursosController.java
+++ src/main/java/br/com/sis/herokudemo/controllers/CursosController.java
@@ -3,7 +3,7 @@ package br.com.sis.herokudemo.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,24 +12,16 @@ import org.springframework.web.servlet.ModelAndView;
 import br.com.sis.herokudemo.model.Curso;
 import br.com.sis.herokudemo.services.CursosService;
 
-@RequestMapping("/cursos")
 @RestController
 public class CursosController {
 
 	@Autowired
 	private CursosService cursosService;
 
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping("/cursos")
 	public List<Curso> hello() {
 
 		return cursosService.listAll();
-	}
-
-	@RequestMapping(value = "/add/{nome}", method = RequestMethod.GET)
-	public String add(@PathVariable String nome) {
-		Curso curso = new Curso(nome);
-		cursosService.salvar(curso);
-		return "Curso " + nome + " foi adicionado";
 	}
 
 	@RequestMapping(value = "/add", method = RequestMethod.GET)
